5631eb1d09e1234c9df8d6814a1212293e0d6fa2
Maks Kravchenko <123405337+Kravazf@users.noreply.github.com>
2024-04-28 17:44:56 +0300

    bags-logo-footer-done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
         <div className='footerCard flex'>
           <div className='footerIntro flex'>
             <div className='logoDiv'>
-              <a href='/' className='logo flex'>
+              <a href='#' className='logo flex'>
                 <MdOutlineTravelExplore className='icon'/>  Travel.
               </a>
             </div>
